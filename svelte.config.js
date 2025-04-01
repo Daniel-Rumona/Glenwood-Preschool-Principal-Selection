52a5c6df9034cd1985353de862f231d814a172a3
Daniel-Rumona <danielrumona@gmail.com>
2025-04-01 17:17:44 +0200

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,27 +1,21 @@
-import adapter from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+  preprocess: vitePreprocess(),
 
-	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter({
-			// default options are shown
-			out: 'build',
-			precompress: false,
-			envPrefix: '',
-			polyfill: true
-		}),
-		alias: {
-			"@/*": "./path/to/lib/*",
-		}
-	}
+  kit: {
+    adapter: adapter({
+      out: 'build', // specify build output directory
+      precompress: false,
+      envPrefix: '',
+      polyfill: true
+    }),
+    alias: {
+      "@/*": "src/*", // Adjust path to match your project structure
+    }
+  }
 };
 
 export default config;
